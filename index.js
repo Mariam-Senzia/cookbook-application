439f589d3c906a21521eeb8ec720113d0951dcbb
Mariam Senzia <mariamsenzia@gmail.com>
2024-01-20 14:38:56 +0300

    Added comments to my code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded",() => {
     const form = document.getElementById("form")
     const input = document.getElementById("inputField")
 
+    //////add event lister to form 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
@@ -15,19 +16,18 @@ document.addEventListener("DOMContentLoaded",() => {
 })
 
 const fetchRecipe = () => {
+    //////fetch data from API
     api =  'https://dummyjson.com/recipes'
     fetch(api) 
     .then((res) => res.json())
     .then((data) => {
-        //console.log(data)
-
         recipes = data.recipes
-        console.log(recipes)
-       
+        //console.log(recipes)
 
         const cuisines = document.getElementById("cuisines")
         const details = document.getElementById("cuisine-data")
 
+        ////////loop through recipes
         recipes.forEach((recipe) => {
             console.log(recipe.cuisine)
             const button1 = document.createElement("button")
@@ -35,28 +35,29 @@ const fetchRecipe = () => {
             button1.id = "button"
             cuisines.append(button1)
 
+            //////add event lister to button
             button1.addEventListener("click", (e) => {
                 e.preventDefault();
-
+                //////append name
                 const h3 = document.createElement("h2")
                 h3.innerText = recipe.name
                 details.appendChild(h3)
-
+                //////append ingredients
                 ing = document.createElement("h4")
                 ing.innerText = `INGREDIENTS: ${recipe.ingredients}`;
                 details.appendChild(ing)
-
+                /////append instructions
                 method = document.createElement("h4")
                 method.innerText = `INSTRUCTIONS:`
                 details.appendChild(method)
-
+                //////loop through instructions to list each instruction
                 const instructions = recipe.instructions
                 instructions.forEach((element) => {
                 instru = document.createElement("li")
                 instru.innerText = element
                 details.appendChild(instru)
                 })
-
+                /////append image 
                 const img = document.createElement("img")
                 img.src = recipe.image
                 img.id = ("image")
